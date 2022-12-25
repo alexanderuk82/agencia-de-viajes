23dd4c1c5525057e1553795e952d835b3624fa39
Alexander <info@alexandersstudio.uk>
2022-12-25 05:22:11 +0000

    New data sequelize

--- config/db.js
+++ config/db.js
@@ -2,20 +2,36 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv/config';
 console.log(process.env.DB_HOST);
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: '3306',
-  dialect: 'mysql',
-  define: {
-    timestamps: false,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  operatorAliases: false,
-});
+const op = Sequelize.Op;
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: '3306',
+    dialect: 'mysql',
+    define: {
+      timestamps: false,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+
+    operatorAliases: {
+      $and: op.and,
+      $or: op.or,
+      $eq: op.eq,
+      $gt: op.gt,
+      $lt: op.lt,
+      $lte: op.lte,
+      $like: op.like,
+    },
+  }
+);
 
 export default db;
